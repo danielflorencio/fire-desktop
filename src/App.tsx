ca983b33fcfb67eb10f46b116fdcee88ddbf9844
danielflorencio <danielflorencio20@gmail.com>
2022-11-22 17:41:17 -0300

    started the new repository

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Button } from 'react-bootstrap';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Test</h1>
+      <Button variant='primary'>Primary</Button>
+    </>
+  )
+}
+
+export default App
