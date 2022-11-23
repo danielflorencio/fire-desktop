a8c7cc31105aa06f07453cc81477a3a627bc18c0
danielflorencio <danielflorencio20@gmail.com>
2022-11-23 00:20:10 -0300

    inserting some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { Button } from 'react-bootstrap';
+import Navbar from './components/Navbar/Navbar';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Test</h1>
-      <Button variant='primary'>Primary</Button>
+      <Navbar/> 
+      <main>
+        <div className='main-content-container'>
+          <h1>Test</h1>
+          <Button variant='primary'>Primary</Button>
+        </div>
+      </main>
     </>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,15 @@
-export default function Navbar(){
+import './styles.css'
+// function NavItem(string linkTo){
+//     return 
+//         <li className="nav-item">
+//             {props.children}
+//         </li>
+// }
+
+export default function Navbar({}){
     return(
-        <nav>
-            <ul>
-                <li></li>
+        <nav className="navbar">
+            <ul className="flex flex-column align-center">
                 <li></li>
                 <li></li>
             </ul>
