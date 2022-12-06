62b3e4377e4fc7119fab96ea98151848e6dd207c
danielflorencio <danielflorencio20@gmail.com>
2022-12-06 11:21:29 -0300

    debugging like a prooooo

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -4,27 +4,33 @@ export const getCurrentMonth = (): string => {
     let newDate = now.getFullYear()-(now.getMonth()+1);
     let stringDate = newDate.toString();
 
-    return stringDate 
+    return `${now.getFullYear()}-${now.getMonth()+1}`
     // return toString((now.getFullYear()-(now.getMonth()+1)))
     // toString(now.getFullYear()-(now.getMonth()+1))
 }
 
 export const filterListByMonth = (list: Expense[], date: string): Expense[] => { 
     let newList: Expense[] = [];
+    
     let [year, month] = date.split('-');
     
-    for(let i = 0; i < list.length; i++){
+    // for(let i = 0; i < list.length; i++){
+    for (let i in list){
+        console.log('for loop in filterlist being called.')
+        console.log('list item number ', i, ' year === ', list[i].date.getFullYear())
+        console.log('list item number ', i, ' month === ', (list[i].date.getMonth() + 1))
         if(
             list[i].date.getFullYear() === parseInt(year) &&
             (list[i].date.getMonth() + 1) === parseInt(month)
         ) {
+            console.log('conditional in for loop being true.')
             newList.push(list[i])
-            console.log('newList ', i, ' ', newList)
+            // console.log('newList ', i, ' ', newList)
         }
     }
     // console.log('date ', Date())
     // console.log('newlist ', newList)
-    console.log(newList)
+    // console.log(newList)
     return newList;
 }
 
