2ce81d66449d4304343f3287dc492b3fba5bca1a
danielflorencio <danielflorencio20@gmail.com>
2022-12-07 11:42:13 -0300

    made the dates work in the expense tracker

--- src/components/TableArea/ExpenseTrackerTable.tsx
+++ src/components/TableArea/ExpenseTrackerTable.tsx
@@ -1,8 +1,9 @@
 import './styles.css'
 import { Expense } from '../../types/expense'
+import { formatDate } from '../../helpers/dateFilter'
 
 type Props = { // Everytime we pass props in React Typescript components, we have to declare the type of the props separately.
-    list: Expense[]
+    list: Expense[] 
 }
 
 const TableItem = ({list}: Props) => {
@@ -31,7 +32,7 @@ export const ExpensesTable = ({list}: Props) => {
                 {
                     list.map((expense, index) =>(
                         <div key={index} className='table-row'>
-                            <div className='table-item-container'><div className='table-item'>{}</div></div>
+                            <div className='table-item-container'><div className='table-item'>{formatDate(expense.date)}</div></div> {/* Note: it is important to remember that the value for month showed in the date prop must be shown with one added. */}
                             <div className='table-item-container'><div className='table-item'>{expense.category}</div></div>
                             <div className='table-item-container'><div className='table-item'>{expense.title}</div></div>
                             <div className='table-item-container'><div className='table-item'>{expense.value}</div></div>
--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,12 +1,7 @@
 import { Expense } from "../types/expense";
 export const getCurrentMonth = (): string => {
     let now = new Date();
-    // let newDate = now.getFullYear()-(now.getMonth()+1);
-    // let stringDate = newDate.toString();
-
     return `${now.getFullYear()}-${now.getMonth()+1}`
-    // return toString((now.getFullYear()-(now.getMonth()+1)))
-    // toString(now.getFullYear()-(now.getMonth()+1))
 }
 
 export const filterListByMonth = (list: Expense[], date: string): Expense[] => { 
@@ -14,19 +9,18 @@ export const filterListByMonth = (list: Expense[], date: string): Expense[] => {
     
     let [year, month] = date.split('-');
     
-    // for(let i = 0; i < list.length; i++){
     for (let i in list){
-        console.log('--------------------------------------')
-        console.log('list item number ', i, ' year === ', list[i].date.getFullYear())
-        console.log('list item number ', i, ' month === ', list[i].date.getMonth())
-        console.log('full date ', list[i].date.getDate())
-        console.log('year variable value === ', year)
-        console.log('month variable value === ' , (parseInt(month) - 1))
+        // console.log('--------------------------------------')
+        // console.log('list item number ', i, ' year === ', list[i].date.getFullYear())
+        // console.log('list item number ', i, ' month === ', list[i].date.getMonth())
+        // console.log('full date ', list[i].date.getDate())
+        // console.log('year variable value === ', year)
+        // console.log('month variable value === ' , (parseInt(month) - 1))
         if(
             (list[i].date.getFullYear()) === parseInt(year) &&
             list[i].date.getMonth()  === (parseInt(month) - 1)
         ) {
-            console.log('conditional in for loop being true.')
+            // console.log('conditional in for loop being true.')
             newList.push(list[i])
             // console.log('newList ', i, ' ', newList)
         }
@@ -39,3 +33,11 @@ export const filterListByMonth = (list: Expense[], date: string): Expense[] => {
 
 // Typescript function skeleton
 // export const name = (variable: type, variable: type): return type => {actual function} 
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+}
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;
