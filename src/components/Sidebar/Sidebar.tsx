af0aa38723a84dc5bb915a6d4bde4a4649f8a931
danielflorencio <danielflorencio20@gmail.com>
2022-12-18 16:22:09 -0300

    put the icons in the sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,30 +1,34 @@
 import './styles.css'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 import { Children, Component } from 'react';
+import { AiFillHome } from 'react-icons/ai'
+import { FaMoneyBillWave } from 'react-icons/fa'
+import {AiFillCalculator, AiFillFire} from 'react-icons/ai'
+
 export default function Sidebar(){
     return(
         <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar-width-280px overflow-hidden">
             <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <svg className="bi pe-none me-2" width="40" height="32"></svg>
+                <svg className="bi pe-none me-2" width="16" height="16"><AiFillFire/></svg>
                 <span className="fs-4">Fire</span>
             </a>
             <hr/>
             <ul className="nav nav-pills flex-column mb-auto">
             <li className="nav-item">
                 <a href="/" className="nav-link active" aria-current="page">
-                <svg className="bi pe-none me-2" width="16" height="16"></svg>
+                <svg className="bi pe-none me-2" width="16" height="16"><AiFillHome/></svg>
                 Home
                 </a>
             </li>
             <li>
                 <a href="/expenses" className="nav-link text-white">
-                <svg className="bi pe-none me-2" width="16" height="16"></svg>
+                <svg className="bi pe-none me-2" width="16" height="16"><FaMoneyBillWave/></svg>
                 Expenses
                 </a>
             </li>
             <li>
                 <a href="/CompoundInterestCalculator" className="nav-link text-white">
-                <svg className="bi pe-none me-2" width="16" height="16"></svg>
+                <svg className="bi pe-none me-2" width="16" height="16"><AiFillCalculator/></svg>
                 Calculators
                 </a>
             </li>
@@ -34,12 +38,12 @@ export default function Sidebar(){
                 Productivity Booster
                 </a>
             </li> */}
-            <li>
+            {/* <li>
                 <a href="#" className="nav-link text-white">
                 <svg className="bi pe-none me-2" width="16" height="16"></svg>
                 Feedback
                 </a>
-            </li>
+            </li> */}
             {/* <li>
                 <a href="#" className="nav-link text-white">
                 <svg className="bi pe-none me-2" width="16" height="16"></svg>
