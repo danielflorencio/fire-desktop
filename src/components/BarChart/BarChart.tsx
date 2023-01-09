e8aeb12a2c42aa3a52ff79cefec1564238d249d6
danielflorencio <danielflorencio20@gmail.com>
2023-01-09 20:14:05 -0300

    cleaned some useless code

--- src/components/BarChart/BarChart.tsx
+++
@@ -1,6 +0,0 @@
-import { Line } from 'react-chartjs-2'
-import { Bar } from 'react-chartjs-2'
-import { Chart as ChartJS } from 'chart.js/auto'
-export default function BarChart({chartData}: any){
-    return <Bar data={chartData}/>
-}
