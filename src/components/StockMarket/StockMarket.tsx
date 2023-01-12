a657e631b3c267283239a3897976ba199bd647f0
danielflorencio <danielflorencio20@gmail.com>
2023-01-12 02:54:18 -0300

    finished the StockMarket component

--- src/components/StockMarket/StockMarket.tsx
+++ src/components/StockMarket/StockMarket.tsx
@@ -13,54 +13,37 @@ export default function StockMarket(){
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<any>(null);
 
-    // const symbols = ['MSFT', 'NFLX', 'STRIPE', 'SHOP', 'META', 'AMZN', 'GOOGL', 'UBER', 'TSLA', 'AAPL'];
-    const symbols = ['MSFT', 'NFLX']
+    // const symbols = ['MSFT', 'NFLX', 'SHOP', 'META', 'AMZN', 'GOOGL', 'UBER', 'TSLA', 'AAPL'];
+    const symbols = ['MSFT', 'NFLX', 'SHOP']
 
 
     useEffect(() => {
         const apiKey = '';
-        // const apiKey = '';
         const fetchStockData = async () => {
           setIsLoading(true);
           setError(null);
     
           try {
             const requests = symbols.map(symbol =>
-              fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`)
+            fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`)
             );
-            console.log('requests: ', requests)
             const responses = await Promise.all(requests);
-            console.log('responses: ', responses)
-            const data = await Promise.all(responses.map(response => response.json()));
-            console.log("data: ", data)
-            
-            
+            const data = await Promise.all(responses.map(response => response.json()));            
             setStockData(
-              data.map((item): any => ({
-                
+              data.map((item): any => ({      
                 symbol: item['Global Quote']['01. symbol'],
                 price: parseFloat(item['Global Quote']['05. price']),
                 change: parseFloat(item['Global Quote']['09. change']),
                 changePercent: parseFloat(item['Global Quote']['10. change percent'].slice(0, -1))
-                // changePercent: item['Global Quote']['10. change percent']
               }))
             );
-
-
-
-
-
-
-            console.log('Stock Data: ', stockData)
             setIsLoading(false);
           } catch (error) {
             setError(error);
-            console.log('Error: ', error)
           }
         };
-    
-        fetchStockData();
-      }, []);
+      fetchStockData();
+    }, []);
     
       if (error) {
         return <div>An error occurred: {error.message}</div>;
@@ -70,16 +53,25 @@ export default function StockMarket(){
         return <div>Loading...</div>;
       }
 
-    return (
-    <ul>
-        {stockData.map(stock => (
-        <li key={stock.symbol}>
-            <div>Symbol: {stock.symbol}</div>
-            <div>Price: {stock.price}</div>
-            <div>Change: {stock.change}</div>
-            <div>Change %: {stock.changePercent}</div>
-        </li>
-        ))}
-    </ul>
+      return (
+        <div className='w-100'>
+          <div style={{marginBottom: '6vh'}} className='d-flex justify-content-center align-items-center'><h1>Stocks</h1></div>
+          <div style={{height: "6vh"}} className='d-flex align-items-center rounded border w-100 justify-content-around'>
+            <div>Stock</div>
+            <div>Price</div>
+            <div>Change</div>
+            <div>Change %</div>
+          </div>
+          {
+            stockData.map(stock => (
+              <div style={{height: "6vh", marginTop: '2vh'}} className='d-flex align-items-center rounded border w-100 justify-content-around'>
+                <div>{stock.symbol}</div>
+                <div>{stock.price}</div>
+                <div className={stock.change > 0 ? 'text-success' : 'text-danger'}>{stock.change}</div>
+                <div className={stock.change > 0 ? 'text-success' : 'text-danger'}>{stock.changePercent}</div>
+              </div>    
+            ))
+          }
+        </div>        
     );
 }
