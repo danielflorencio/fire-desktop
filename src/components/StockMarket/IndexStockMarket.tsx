af00367d21809c582bb037023ba1eb291326fa75
danielflorencio <danielflorencio20@gmail.com>
2023-01-26 05:41:00 -0300

    put the video in the stockmarket feature

--- src/components/StockMarket/IndexStockMarket.tsx
+++ src/components/StockMarket/IndexStockMarket.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import ChooseStocks from "./ChooseStocks";
-import ShowStocks from "./ShowStockMarket";
+// import ChooseStocks from "./ChooseStocks";
+// import ShowStocks from "./ShowStockMarket";
+import { useMediaQuery } from 'react-responsive'
 export interface StockData {
     symbol: string;
     price: number;
@@ -8,13 +9,16 @@ export interface StockData {
     changePercent: number;
 }
 export default function IndexStockMarket(){
+    const isSmallScreen = useMediaQuery({ query: '(max-width: 568px)' });
     const [stocksToSearch, setStocksToSearch] = useState<StockData[]>([])
     const apiKey = '';
 
     return(
         <>
-            <ChooseStocks stocksToSearch={stocksToSearch} setStocksToSearch={setStocksToSearch} apiKey={apiKey}/>
-            <ShowStocks stocksToSearch={stocksToSearch} apiKey={apiKey}/>
+            <h2 className="text-center mt-4">Hey, i didn't let the feature working to not expose my API Key. Here's a video showing it live: </h2>
+            <div className="mt-5"><iframe width={isSmallScreen ? "400" : "560"} height={isSmallScreen ? "220" : "315"} src="https://www.youtube.com/embed/C4atterMyF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+            {/* <ChooseStocks stocksToSearch={stocksToSearch} setStocksToSearch={setStocksToSearch} apiKey={apiKey}/>
+            <ShowStocks stocksToSearch={stocksToSearch} apiKey={apiKey}/> */}
         </>
     )
 }
