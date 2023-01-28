9c0b0b41be05ea340c7442bb9f3ddd785700fc2d
danielflorencio <danielflorencio20@gmail.com>
2023-01-28 10:48:17 -0300

    made the addExpenseForm responsive

--- src/components/AddExpenseForm/AddExpenseForm.tsx
+++ src/components/AddExpenseForm/AddExpenseForm.tsx
@@ -5,11 +5,11 @@ import { newDateAdjusted } from '../../helpers/dateFilter';
 import { useMediaQuery } from 'react-responsive';
 
 export default function AddExpense({ onAdd }: {onAdd: (expense: Expense) => void}){
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 968px)' });
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 767px)' });
   const [dateField, setDateField] = useState('');
   const [categoryField, setCategoryField] = useState('');
   const [titleField, setTitleField] = useState('');
-  const [valueField, setValueField] = useState(0);
+  const [valueField, setValueField] = useState<number>(0);
 
   let categoryKeys: string[] = Object.keys(categories);
 
@@ -46,10 +46,59 @@ export default function AddExpense({ onAdd }: {onAdd: (expense: Expense) => void
     setTitleField('');
     setValueField(0);
   }    
-  
-  return(
-      <div className='info-area-container d-flex align-items-center flex-wrap' style={{padding: '1vh 1vw'}}>
-        <div className='d-flex justify-content-around w-100'>
+
+  if(isSmallScreen){
+    return(
+      <>
+        <div className='info-area-container d-flex align-items-center flex-wrap' style={{padding: '1vh 1vw'}}>
+          
+          <div className='d-flex flex-column px-2'>
+            <div className='mb-3'>
+            <label><strong>Date: </strong></label>
+              <input type="date" className='w-100 text-center' value={dateField} onChange={e => setDateField(e.target.value)}></input>
+            </div>
+
+            <div className='mb-3'>
+            <label><strong>Category: </strong></label>
+            <select className='w-100 text-center' value={categoryField} onChange={e => setCategoryField(e.target.value)}>
+                <option>Choose one</option>
+                {categoryKeys.map(key => (
+                  <option key={key} value={key}>{categories[key].title}</option>
+                  ))
+                }
+            </select>
+            </div>
+
+            <div className='mb-3'>
+            <label className='d-block'><strong>Title: </strong></label>
+            <input type="text" className="w-100" value={titleField} onChange={e => setTitleField(e.target.value)}></input>
+            </div>
+
+            <div className='mb-3'>
+            <label><strong>Value: </strong></label>
+            <input type="number" className='w-100' value={valueField} onChange={e => setValueField(parseFloat(e.target.value))}></input>
+            </div>
+          </div>
+              
+          
+
+          
+              
+              
+          
+          <div className='w-100 d-flex align-items-center justify-content-center'>
+              
+            <br></br>
+            <button className="btn btn-primary" onClick={handleAddEvent}>Add Expense</button>
+              
+          </div>
+      </div>
+      
+      </>
+    )
+  } else{
+    return(
+        <div className='info-area-container d-flex align-items-center flex-wrap' style={{padding: '1vh 1vw'}}>
           <div>
               <div className='text-center'><strong>Date</strong></div>
               <input type="date" value={dateField} onChange={e => setDateField(e.target.value)}></input>
@@ -58,7 +107,7 @@ export default function AddExpense({ onAdd }: {onAdd: (expense: Expense) => void
               <div><strong>Category</strong></div>
               <label>
                   <select value={categoryField} onChange={e => setCategoryField(e.target.value)}>
-                      <option></option>
+                      <option>Category</option>
                       {categoryKeys.map(key => (
                         <option key={key} value={key}>{categories[key].title}</option>
                         ))
@@ -66,11 +115,10 @@ export default function AddExpense({ onAdd }: {onAdd: (expense: Expense) => void
                   </select>
               </label>
           </div>
-        </div>
-        <div className='d-flex justify-content-around w-100 align-items-center'>
+
           <div>
               <div className='text-center'><strong>Title</strong></div>
-              <input type="text" className="w-8rem" value={titleField} onChange={e => setTitleField(e.target.value)}></input>
+              <input type="text" className="w-8rem" placeholder='value' value={titleField} onChange={e => setTitleField(e.target.value)}></input>
           </div>
           <div>
               <div className='text-center'><label><strong>Value</strong></label></div>
@@ -82,7 +130,9 @@ export default function AddExpense({ onAdd }: {onAdd: (expense: Expense) => void
                   <button className="btn btn-primary" onClick={handleAddEvent}>Add Expense</button>
               </label>
           </div>
-        </div>
       </div>
-  )
+    )
+  }
+  
+  
 }
